d21e2172509e10d16377225ca80f817ab76768cd
ChiaraPontello <chiarapontello1@gmail.com>
2023-10-17 15:50:12 +0200

    pari o dispari scelta

--- pari e dispari/script.js
+++ pari e dispari/script.js
@@ -8,4 +8,8 @@ function pari (number) {
   function dispari(number) {
     return number % 2 !== 0;
   }
+  // chiedere se il numero scelto è pari o dispari 
+  let miaScelta = prompt('pari o dispari?');
+  console.log('la tua scelta è stata:' + ' ' + miaScelta);
+  
   
