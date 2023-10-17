d7bba34c94df994f69e2b3720400b4b9b35178da
ChiaraPontello <chiarapontello1@gmail.com>
2023-10-17 16:07:22 +0200

    ciclo pari o dispari

--- pari e dispari/script.js
+++ pari e dispari/script.js
@@ -10,6 +10,35 @@ function pari (number) {
   }
   // chiedere se il numero scelto è pari o dispari 
   let miaScelta = prompt('pari o dispari?');
-  console.log('la tua scelta è stata:' + ' ' + miaScelta);
+  console.log('la tua scelta è:' + ' ' + miaScelta);
+  
+  //iniziare la funzione con la scelta dell'ultente
+  let mioNumero;
+  
+  // chiedere di inseire un numero da 1 a 5
+  while (true) {
+    mioNumero = parseInt(prompt('scegli un numero da 1 a 5'));
+  
+    if ( mioNumero >= 1 && mioNumero <= 5) {
+      console.log(true)
+    } else {
+      alert('scelgo un numero da 1 a 5');
+    }console.log('il mio numero è:' + ' ' +mioNumero);
+  }
   
   
+  // funzione 2: pc
+  let pcNum = Math.floor(Math.random() * 5) + 1;
+  console.log('pc numero:' + ' ' + pcNum)
+  
+  // somma
+  let somma = mioNumero + pcNum;
+  console.log(somma)
+  
+  // se esce pari o dispari
+  if (miaScelta === pari && somma % 2 === 0) {
+    console.log('Ho vinto!'); 
+  } else {
+    console.log('Hai perso');
+  
+  }
